a39887e1918b2211c7abfe481f9aa2ad32ed1e92
Anthony Drake <50809386+AntEDra@users.noreply.github.com>
2021-04-14 20:11:22 -0400

    updated pipline
    spider data should go into a table now

--- TNDReviews/TNDReviews/pipelines.py
+++ TNDReviews/TNDReviews/pipelines.py
@@ -5,9 +5,47 @@
 
 
 # useful for handling different item types with a single interface
+import sqlite3
 from itemadapter import ItemAdapter
 
 
-class TndreviewsPipeline:
-    def process_item(self, item, spider):
-        return item
+class TheneedledropPipeline:
+
+	def __init__(self):
+		self.create_connetion()
+		self.create_table()
+
+	def create_connetion(self):
+		self.conn = sqlite3.connect("fantanoScores.db")
+		self.curr = self.conn.cursor()
+
+	def create_table(self):
+
+		self.curr.execute("""DROP TABLE IF EXISTS reviews_tb""")		
+
+		self.curr.execute("""create table reviews_tb(
+								url text,
+								date text,
+								artist text,
+								album text,
+								name text,
+								score integer,
+								tags text)""")
+
+	def process_item(self, item, spider):
+
+		self.store_db(item)
+		return item
+
+	def store_db(self,item):
+
+		self.curr.execute("""insert into reviews_tb values (?,?,?,?,?,?,?)""",(
+    		item['url'],
+    		item['date'],
+    		item['artist'],
+    		item['album'],
+    		item['name'],
+    		item['score'],
+    		item['tags']
+    		))
+		self.conn.commit()
